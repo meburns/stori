0097f35782ca88ca703fa8d182d0e7f0adf536ca
kee <keelin@iastate.edu>
2019-06-19 03:58:55 -0500

    Add Role model

---
+++ Backend/stori/src/main/java/com/stori/model/User.java
@@ -0,0 +1,90 @@
+package com.stori.model;
+
+import java.sql.Date;
+import java.util.HashSet;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.Email;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.mapping.Set;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+		@UniqueConstraint(columnNames = {
+				"username"
+		}),
+		@UniqueConstraint(columnNames = {
+				"email"
+		})
+}) 
+
+public class User {
+
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotFound(action = NotFoundAction.EXCEPTION)
+	private long id;
+
+	@Column(name = "username")
+	@NotFound(action = NotFoundAction.EXCEPTION)
+	private String username;
+
+	@Column(name = "email")
+	@NotFound(action = NotFoundAction.EXCEPTION)
+	@Email 
+	private String email;
+
+	@Column(name = "password")
+	@NotFound(action = NotFoundAction.EXCEPTION)
+	private String password;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "user_roles", 
+		joinColumns = @JoinColumn(name = "user_id"), 
+		inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
+	@CreationTimestamp
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created_at")
+	private Date created_at;
+
+	@UpdateTimestamp
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "updated_at")
+	private Date updated_at;
+
+	public User() {}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
